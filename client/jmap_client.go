025b1d509e0e06c2726a67eb89aeca6cb6bb6ebe
Manuel Stößel <manuel.stoessel@gmail.com>
2020-07-03 12:52:31 +0200

    Refactor client code and add working basic auth

--- client/jmap_client.go
+++ client/jmap_client.go
@@ -1,7 +1,6 @@
 package client
 
 import (
-	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"time"
@@ -14,35 +13,8 @@ type JMAPClient struct {
 	Hostname string
 	Username string
 	Password string
-}
-
-func (c *JMAPClient) getAuthURL() string {
-	return c.Hostname + "/.well-known/jmap"
-}
-
-func (c *JMAPClient) getAuthToken() string {
-	token := []byte(c.Username + ":" + c.Password + ":")
-	return base64.StdEncoding.EncodeToString(token)
-}
-
-// GetSession fills a Session struct with the response from a qualified JMAP
-// APIs .well-known/jmap endpoint
-func (c *JMAPClient) GetSession(s *models.Session) error {
-	client := &http.Client{Timeout: 10 * time.Second}
-
-	req, err := http.NewRequest("GET", c.getAuthURL(), nil)
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Authorization", "Basic "+c.getAuthToken())
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	return json.NewDecoder(resp.Body).Decode(s)
+	Client   *http.Client
+	Session  *models.Session
 }
 
 // NewJMAPClient returns a pointer to a newly created JMAPClient
@@ -51,5 +23,30 @@ func NewJMAPClient(host string, user string, pw string) *JMAPClient {
 		Hostname: host,
 		Username: user,
 		Password: pw,
+		Client:   &http.Client{Timeout: 10 * time.Second},
+		Session:  &models.Session{},
 	}
+}
+
+// GetSession fills a Session struct with the response from a qualified JMAP
+// APIs .well-known/jmap endpoint
+func (c *JMAPClient) GetSession() error {
+
+	req, err := http.NewRequest("GET", c.getAuthURL(), nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.SetBasicAuth(c.Username, c.Password)
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return json.NewDecoder(resp.Body).Decode(c.Session)
+}
+
+func (c *JMAPClient) getAuthURL() string {
+	return c.Hostname + "/.well-known/jmap"
 }
