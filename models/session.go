bcb13024a1857069414020dc301c88d6789fc460
Manuel Stößel <manuel@loodse.com>
2020-07-06 10:45:11 +0200

    Fix Session structs

--- models/session.go
+++ models/session.go
@@ -3,20 +3,20 @@ package models
 // Session represents a JMAP session object as described here:
 // https://jmap.io/spec-core.html#the-jmap-session-resource
 type Session struct {
-	Capabilities    map[string]Capability `json:"capabilities"`
-	Accounts        map[AccountID]Account `json:"accounts"`
-	PrimaryAccounts map[string]AccountID  `json:"primaryAccounts"`
-	Username        string                `json:"username"`
-	APIUrl          string                `json:"apiUrl"`
-	DownloadURL     string                `json:"downloadUrl"`
-	UploadURL       string                `json:"uploadUrl"`
-	EventSourceURL  string                `json:"eventSourceUrl"`
-	State           string                `json:"state"`
+	Capabilities    map[JMAPCapabilityURN]interface{} `json:"capabilities"`
+	Accounts        map[AccountID]Account             `json:"accounts"`
+	PrimaryAccounts map[string]AccountID              `json:"primaryAccounts"`
+	Username        string                            `json:"username"`
+	APIUrl          string                            `json:"apiUrl"`
+	DownloadURL     string                            `json:"downloadUrl"`
+	UploadURL       string                            `json:"uploadUrl"`
+	EventSourceURL  string                            `json:"eventSourceUrl"`
+	State           string                            `json:"state"`
 }
 
-// Capability is an object describing the JMAP servers capabilities and
-// limits that have a direct influence on client behaviour
-type Capability struct {
+// SessionMailCapability is an object describing the JMAP servers capabilities
+// and limits that have a direct influence on client behaviour for the mail URN
+type SessionMailCapability struct {
 	MaxSizeUpload         uint64   `json:"maxSizeUpload"`
 	MaxConcurrentUpload   uint64   `json:"maxConcurrentUpload"`
 	MaxSizeRequest        uint64   `json:"maxSizeRequest"`
